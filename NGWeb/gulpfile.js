1abba775995fc4d4fdae7af66afa6ce1b3163723
Subhash Pavuskar <subhash.pavuskar@gmail.com>
2025-08-11 20:55:26 +0530

    Update gulpfile.js

--- NGWeb/gulpfile.js
+++ NGWeb/gulpfile.js
@@ -23,7 +23,6 @@ gulp.task('copy-web-resources', [], function () {
 });
 
 
-
 gulp.task('copy-data-resources', [], function () {
     gulp.src(['app/data/*'])
         .pipe(gulp.dest('build/data/'));
